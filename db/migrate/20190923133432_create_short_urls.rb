c260c07e1a6df872805146044f78d165371cc737
Rodrigo Solis <rodrigo.solisgonzalez@outlook.com>
2022-10-29 00:22:44 -0600

    Project Setup

---
+++ db/migrate/20190923133432_create_short_urls.rb
@@ -0,0 +1,12 @@
+class CreateShortUrls < ActiveRecord::Migration[6.0]
+  def change
+    create_table :short_urls do |t|
+      t.string :full_url, index: true
+      t.string :title
+      t.string :short_url
+      t.integer :click_count, default: 0
+
+      t.timestamps
+    end
+  end
+end
