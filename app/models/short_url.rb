4a114cbfdd0bc9bad2171653f8795e79e4a6405a
Rodrigo Solis <rodrigo.solisgonzalez@outlook.com>
2022-10-29 03:27:08 -0600

    CHL-6
    BugFix at validation url method

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -21,14 +21,14 @@ class ShortUrl < ApplicationRecord
   private
 
   def url_validator?(url)
-    uri = URL.parse(url)
+    uri = URI.parse(url)
     uri.is_a?(URI::HTTP) && !uri.host.nil?
     rescue URI::InvalidURIError
     false
   end
 
   def validate_full_url
-    isValid = url_validator(full_url)
+    isValid = url_validator?(full_url)
     if !isValid
       errors.add(:full_url, "isn't a valid URL")
     end
